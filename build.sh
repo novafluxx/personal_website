33b626aec2cdb3b04f9e966a8177e74ab95e1f53
Justin High <novafluxx@users.noreply.github.com>
2024-11-16 22:26:07 -0500

    Still trying to get this to work

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet workload install wasm-tools
-./dotnet workload restore
-./dotnet/dotnet publish -c Release -o output
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
+chmod +x dotnet-install.sh;
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet;
+./dotnet/dotnet --version;
+./dotnet workload install wasm-tools;
+./dotnet workload restore;
+./dotnet/dotnet publish -c Release -o output;
