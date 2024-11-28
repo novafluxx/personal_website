087151ef6945184ae57000b72ef5d9e74bfccfef
Justin High <novafluxx@users.noreply.github.com>
2024-11-27 22:30:16 -0500

    Implemented Lazy Loading

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 using personal_website.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,6 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Configure lazy loading
+builder.Services.AddScoped<LazyAssemblyLoader>();
 
 try
 {
